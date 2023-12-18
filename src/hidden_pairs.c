0905dd9b25fce35e29b35eab25a884daff70a9ab
Dao Hong Minh <10422051@student.vgu.edu.vn>
2023-12-18 18:35:37 +0700

    comit

--- src/hidden_pairs.c
+++ src/hidden_pairs.c
@@ -1,52 +1,63 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "hidden_pairs.h"
+#include <stdlib.h>
 
-// Helper function to check if two sets of candidates are equal
-int are_candidate_sets_equal(int candidates1[], int candidates2[]) {
-    for (int i = 0; i < BOARD_SIZE; i++) {
-        if (candidates1[i] != candidates2[i]) {
-            return 0; // Not equal
-        }
+// Function to check if two cells contain the same pair of candidates.
+int are_cells_hidden_pairs(Cell *cell1, Cell *cell2) {
+    if (cell1->num_candidates != 2 || cell2->num_candidates != 2)
+        return 0;
+
+    for (int i = 0; i < 2; i++) {
+        if (!is_candidate(cell1, cell2->candidates[i]) || !is_candidate(cell2, cell1->candidates[i]))
+            return 0;
     }
-    return 1; // Equal
+
+    return 1;
 }
 
-// Function to find hidden pairs within a set of cells
-void find_hidden_pairs(Cell **p_cells, HiddenPairs *p_hidden_pairs, int *p_counter) {
+// Find hidden pairs in a set of cells.
+int find_hidden_pairs(Cell **p_cells, HiddenPair *p_hidden_pairs, int *p_counter) {
+    int hidden_pairs_found = 0;
+
     for (int i = 0; i < BOARD_SIZE - 1; i++) {
-        if (p_cells[i]->num_candidates == 2) {
-            for (int j = i + 1; j < BOARD_SIZE; j++) {
-                if (p_cells[j]->num_candidates == 2) {
-                    // Check if candidates are the same for both cells
-                    if (are_candidate_sets_equal(p_cells[i]->candidates, p_cells[j]->candidates)) {
-                        p_hidden_pairs[(*p_counter)++] = (HiddenPairs){p_cells[i], p_cells[j]};
-                        break; // found a hidden pair, no need to check further
-                    }
-                }
+        if (p_cells[i]->num_candidates != 2)
+            continue;
+
+        for (int j = i + 1; j < BOARD_SIZE; j++) {
+            if (are_cells_hidden_pairs(p_cells[i], p_cells[j])) {
+                p_hidden_pairs[(*p_counter)].p_cells = p_cells;
+                p_hidden_pairs[(*p_counter)].indices[0] = i;
+                p_hidden_pairs[(*p_counter)].indices[1] = j;
+                p_hidden_pairs[(*p_counter)].values[0] = p_cells[i]->candidates[0];
+                p_hidden_pairs[(*p_counter)].values[1] = p_cells[i]->candidates[1];
+                (*p_counter)++;
+                hidden_pairs_found = 1;
+                break;
             }
         }
     }
+
+    return hidden_pairs_found;
 }
 
-// Function to find hidden pairs in a row, column, or box
+// Apply hidden pairs strategy to a Sudoku board.
 int hidden_pairs(SudokuBoard *p_board) {
-    int counter = 0;
-    HiddenPairs p_hidden_pairs[BOARD_SIZE * BOARD_SIZE];
+    int hidden_pair_count = 0;
+    HiddenPair hidden_pairs[BOARD_SIZE * BOARD_SIZE];
 
     for (int i = 0; i < BOARD_SIZE; i++) {
-        // Find hidden pairs in rows, columns, and boxes
-        find_hidden_pairs(p_board->p_rows[i], p_hidden_pairs, &counter);
-        find_hidden_pairs(p_board->p_cols[i], p_hidden_pairs, &counter);
-        find_hidden_pairs(p_board->p_boxes[i], p_hidden_pairs, &counter);
+        hidden_pair_count += find_hidden_pairs(p_board->p_rows[i], &hidden_pairs[hidden_pair_count], &hidden_pair_count);
+        hidden_pair_count += find_hidden_pairs(p_board->p_cols[i], &hidden_pairs[hidden_pair_count], &hidden_pair_count);
+        hidden_pair_count += find_hidden_pairs(p_board->p_boxes[i], &hidden_pairs[hidden_pair_count], &hidden_pair_count);
     }
 
-        for (int i = 0; i < counter; i++)
-    {
-        Cell** p_cells = p_hidden_pairs[i].p_cells;
-        set_candidates(p_cells[p_hidden_pairs[i].index], p_hidden_pairs[i].values, 2);
+    for (int i = 0; i < hidden_pair_count; i++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            if (j != hidden_pairs[i].indices[0] && j != hidden_pairs[i].indices[1]) {
+                unset_candidate(hidden_pairs[i].p_cells[j], hidden_pairs[i].values[0]);
+                unset_candidate(hidden_pairs[i].p_cells[j], hidden_pairs[i].values[1]);
+            }
+        }
     }
 
-
-    return counter;
+    return hidden_pair_count;
 }
--- src/hidden_pairs.h
+++ src/hidden_pairs.h
@@ -4,13 +4,13 @@
 
 typedef struct HiddenPair_impl
 {
-    Cell *p_cells[2];
-    int index;
+    Cell **p_cells;
+    int indices[2];
     int values[2];
-} HiddenPairs;
+} HiddenPair;
 
-// Find hidden pairs in a row, column, or box
-void find_hidden_pairs(Cell **p_cells, HiddenPairs *p_hidden_pairs, int *p_counter);
+// Find hidden pairs in a set of cells.
+int find_hidden_pairs(Cell **p_cells, HiddenPair *p_hidden_pairs, int *p_counter);
 
-// Find hidden pairs in a board, return the number of hidden pairs found
+// Apply hidden pairs strategy to a Sudoku board.
 int hidden_pairs(SudokuBoard *p_board);
